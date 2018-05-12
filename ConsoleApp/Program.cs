2ae532813cd444c9a731a927d00da4400df63a19
AutoIt1080 <kori3132@hotmail.com>
2018-05-12 11:16:32 +0200

    consoleapp

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            x = (x == 16) ? x = 0 : x = 1;
+
+            Console.WriteLine(fun(out x));
+
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+        public static int fun(out int x)
+        {
+            x = 10;
+            return 0;
+        }
+
+        public static Tuple<int, int> NbrOfLaps(int x, int y)
+        {
+            var bobLap = 0;
+            var charlesLap = 0;
+            var bobDistance = 0;
+            var charlesDistance = 0;
+
+            do
+            {
+                if (bobDistance < charlesDistance)
+                {
+                    bobDistance += x;
+                    bobLap++;
+                }
+                else
+                {
+                    charlesDistance += y;
+                    charlesLap++;
+                }
+
+            } while (bobDistance != charlesDistance);
+
+            return new Tuple<int, int>(bobLap, charlesLap);
+        }
+    }
+}
